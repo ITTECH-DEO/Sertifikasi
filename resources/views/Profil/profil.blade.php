98d67280e609faa0f4f3ce05ad6e9e5bf34a28eb
ITTECH-DEO <etechproffit@gmail.com>
2021-10-26 21:28:50 +0700

    First Commit

---
+++ resources/views/Profil/profil.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.master')
+
+@section('content') 
+<div class="container mt-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-7">
+            <div class="card p-3 py-4">
+                <div class="text-center"> <img src="Pas Foto .jpg" width="100" class="rounded-circle"> </div>
+                <div class="text-center mt-3"> <span class="bg-secondary p-1 px-4 rounded text-white">Created By </span>
+                    <h5 class="mt-2 mb-0"></h5> 
+                    <h6>Nama &nbsp;: Deo Fahmawan</h6>
+                    <h6>NIM  &nbsp;: 1931710124</h6>
+                    <h6>Jurusan &nbsp;: Teknologi Informasi</h6>
+                    <h6>Prodi &nbsp;: Manajemn Informatika</h6>
+                    <h6>Tanggal &nbsp;: 25 Oktober 2021</h6>
+                    
+                    <div class="buttons"> <button class="btn btn-outline-primary px-4">Message</button> <button class="btn btn-primary px-4 ms-3">Contact</button> </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+{{-- @endsection
